df887acfb4156b92426ced79298f4082c14a0518
scto <tschmid35@gmail.com>
2025-06-06 23:34:58 +0000

    chore: Format Code on branch master
    fornat code with ktfmt with github actions

--- app/src/main/java/moe/smoothie/androidide/themestore/viewmodels/ThemeActivityViewModel.kt
+++ app/src/main/java/moe/smoothie/androidide/themestore/viewmodels/ThemeActivityViewModel.kt
@@ -1,15 +1,14 @@
 package moe.smoothie.androidide.themestore.viewmodels
 
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import moe.smoothie.androidide.themestore.ThemeState
 import okhttp3.OkHttpClient
-import javax.inject.Inject
 
 @AssistedFactory
 interface ThemeActivityViewModelFactory {
@@ -17,17 +16,14 @@ interface ThemeActivityViewModelFactory {
 }
 
 @HiltViewModel
-class ThemeActivityViewModel @Inject constructor(
-    private val httpClient: OkHttpClient,
-    @Assisted private val url: String
-) : ViewModel() {
+class ThemeActivityViewModel
+@Inject
+constructor(private val httpClient: OkHttpClient, @Assisted private val url: String) : ViewModel() {
     private val _isLoading = MutableStateFlow(false)
     val isLoading: StateFlow<Boolean> = _isLoading
 
     private val _themeState = MutableStateFlow<ThemeState?>(null)
     val themeState: StateFlow<ThemeState?> = _themeState
 
-    fun loadInfo() {
-
-    }
+    fun loadInfo() {}
 }
