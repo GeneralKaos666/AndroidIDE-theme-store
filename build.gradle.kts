a39d21d03723b729a06388eede0d72c0bb7383c8
Thomas Schmid <1441369+scto@users.noreply.github.com>
2025-06-07 03:33:46 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,53 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+import com.android.build.gradle.BaseExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
+import org.jetbrains.kotlin.gradle.dsl.jvm.JvmTargetValidationMode
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.jetbrains.kotlin.android) apply false
-    alias(libs.plugins.compose.compiler) apply false
-    kotlin("kapt") version "2.1.21" apply false
-    id("com.google.dagger.hilt.android") version "2.56.2" apply false
-//    kotlin("jvm") version "2.0.20" apply false
-    kotlin("plugin.serialization") version "2.1.21" apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.android.library) apply false
+  alias(libs.plugins.kotlin) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.aboutlibraries) apply false
 }
+
+buildscript {
+  dependencies { classpath(libs.androidx.navigation.safe.args.gradle.plugin) }
+}
+
+fun Project.configureBaseExtension() {
+  extensions.findByType(BaseExtension::class)?.run {
+    compileSdkVersion(35)
+
+    defaultConfig {
+      minSdk = 26
+      targetSdk = 28
+      versionCode = 100
+      versionName = "1.0.0"
+    }
+
+    compileOptions {
+      sourceCompatibility = JavaVersion.VERSION_17
+      targetCompatibility = JavaVersion.VERSION_17
+    }
+  }
+}
+
+subprojects {
+  plugins.withId("com.android.application") { configureBaseExtension() }
+  plugins.withId("com.android.library") { configureBaseExtension() }
+
+  tasks.withType<KotlinCompile>().configureEach {
+    compilerOptions {
+      apiVersion = KotlinVersion.KOTLIN_2_0
+      languageVersion = KotlinVersion.KOTLIN_2_0
+      jvmTarget = JvmTarget.JVM_17
+      jvmTargetValidationMode = JvmTargetValidationMode.WARNING
+      freeCompilerArgs.add("-Xjvm-default=all")
+    }
+  }
+}
+
+tasks.register<Delete>("clean") { delete(rootProject.layout.buildDirectory) }
